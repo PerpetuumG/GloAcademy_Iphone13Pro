772ad5b30ff41cf6614f70393841ca5c88d48e53
PerpetuumG <perpetuumg@mail.ru>
2022-01-24 18:56:32 +0200

    Day2 + HomeWork2

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,37 +1,32 @@
-const links = document.querySelectorAll('.header-menu__item a')
-const fullChar = document.querySelector('.card-details__link-characteristics')
+const scrollFunc = () => {
 
-seamless.polyfill()
+    const links = document.querySelectorAll('.header-menu__item a')
+    const linkCharacteristics = document.querySelector('.card-details__link-characteristics')
 
-links.forEach((element) => {
-    element.addEventListener('click', (event) => {
-        event.preventDefault()
-        const id = element.getAttribute('href').substring(1)
-        const section = document.getElementById(id)
+    const newArray = [...links, linkCharacteristics]
 
-        if(section) {
-            seamless.elementScrollIntoView(section, {
-                behavior: 'smooth',
-                block: 'start'
-            })
-        } else {
-            seamless.elementScrollIntoView(document.querySelector("#characteristics"), {
-                behavior: "smooth",
-                block: "center",
-                inline: "center",
-            })
-        }
+    seamless.polyfill()
+
+    newArray.forEach((element) => {
+        element.addEventListener('click', (event) => {
+            event.preventDefault()
+            const id = element.getAttribute('href').substring(1)
+            const section = document.getElementById(id)
+
+            if (section) {
+                seamless.elementScrollIntoView(section, {
+                    behavior: 'smooth',
+                    block: 'start'
+                })
+            } else {
+                seamless.elementScrollIntoView(document.querySelector("#characteristics"), {
+                    behavior: "smooth",
+                    block: "center",
+                    inline: "center",
+                })
+            }
+        })
     })
-})
+}
 
-fullChar.addEventListener('click', (event) => {
-    event.preventDefault()
-    const fullCharId = fullChar.getAttribute('href').substring(1)
-    const fullCharSection = document.getElementById(fullCharId)
-
-    seamless.elementScrollIntoView(fullCharSection, {
-        behavior: 'smooth',
-        block: 'start'
-    })
-})
-
+scrollFunc()

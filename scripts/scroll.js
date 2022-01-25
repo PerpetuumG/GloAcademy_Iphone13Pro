df0e61c9cef574eda379a7ccb50c86d3c96a27a1
PerpetuumG <perpetuumg@mail.ru>
2022-01-25 08:09:24 +0200

    add repository

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,32 +1,36 @@
 const scrollFunc = () => {
+    const links = document.querySelectorAll('.header-menu__item a');
+    const allCharater = document.querySelector('.card-details__link-characteristics');
 
-    const links = document.querySelectorAll('.header-menu__item a')
-    const linkCharacteristics = document.querySelector('.card-details__link-characteristics')
+    seamless.polyfill();
 
-    const newArray = [...links, linkCharacteristics]
-
-    seamless.polyfill()
-
-    newArray.forEach((element) => {
+    links.forEach((element) => {
         element.addEventListener('click', (event) => {
-            event.preventDefault()
-            const id = element.getAttribute('href').substring(1)
-            const section = document.getElementById(id)
+            event.preventDefault();
+
+            const id = element.getAttribute('href').substring(1);
+            const section = document.getElementById(id);
 
             if (section) {
                 seamless.elementScrollIntoView(section, {
-                    behavior: 'smooth',
-                    block: 'start'
-                })
-            } else {
-                seamless.elementScrollIntoView(document.querySelector("#characteristics"), {
                     behavior: "smooth",
-                    block: "center",
-                    inline: "center",
+                    block: "start"
                 })
             }
         })
     })
+
+    allCharater.addEventListener('click', (event) => {
+        event.preventDefault();
+
+        const allCharaterId = allCharater.getAttribute('href').substring(1);
+        const allCharaterSection = document.getElementById(allCharaterId);
+
+        seamless.elementScrollIntoView(allCharaterSection, {
+            behavior: "smooth",
+            block: "start"
+        })
+    })
 }
 
-scrollFunc()
+scrollFunc();
--- scripts/tabs.js
+++ scripts/tabs.js
@@ -1,58 +1,41 @@
 const tabsFunc = () => {
-
-    const tabs = document.querySelectorAll('.card-detail__change')
-    const tabsTitle = document.querySelector('.card-details__title')
-    const tabsPrice = document.querySelector('.card-details__price')
-    const tabsImage = document.querySelector('.card__image_item')
-    const title = document.querySelector('title')
+    const tabs = document.querySelectorAll('.card-detail__change');
+    const tabsTitle = document.querySelector('.card-details__title');
+    const tabsPrice = document.querySelector('.card-details__price');
+    const tabsImage = document.querySelector('.card__image_item');
 
     const tabsOptions = [
-        {
-            name: 'Graphite',
-            memory: '128',
-            price: 60000,
-            image: './img/iPhone-graphite.webp'
-        },
-        {
-            name: 'Silver',
-            memory: '256',
-            price: 65000,
-            image: './img/iPhone-silver.webp'
-        },
-        {
-            name: 'Sierra Blue',
-            memory: '512',
-            price: 70000,
-            image: './img/iPhone-sierra_blue.webp'
-        }
+        {name: 'Graphite', memory: '128', price: '99999', image: 'img/iPhone-graphite.webp'},
+        {name: 'Silver', memory: '256', price: '119999', image: 'img/iPhone-silver.webp'},
+        {name: "Sierra Blue", memory: '512', price: '129999', image: 'img/iPhone-sierra_blue.webp'}
     ]
+
     const changeContent = (index) => {
-        tabsTitle.textContent = `Смартфон Apple Phone 13 Pro ${tabsOptions[index].memory}Gb ${tabsOptions[index].name}`
-        tabsPrice.textContent = `${tabsOptions[index].price}₽`
-
-        tabsImage.setAttribute('src', `${tabsOptions[index].image}`)
-
-        title.textContent = `${tabsOptions[index].name}`
+        tabsTitle.innerHTML = `Смартфон Apple iPhone 13 Pro ${tabsOptions[index].memory}GB ${tabsOptions[index].name}`;
+        tabsPrice.innerHTML = `${tabsOptions[index].price}₽`;
+        
+        tabsImage.setAttribute('src', tabsOptions[index].image);
+        document.title = `iPhone 13 Pro ${tabsOptions[index].memory}GB ${tabsOptions[index].name}`;
     }
 
     const changeActiveTabs = (indexClickedTab) => {
         tabs.forEach((tab, index) => {
-            tab.classList.remove('active')
+            tab.classList.remove('active');
 
             if (index === indexClickedTab) {
                 tab.classList.add('active')
             }
         })
-        changeContent(indexClickedTab)
+        changeContent(indexClickedTab);
     }
 
     tabs.forEach((tab, index) => {
         tab.addEventListener('click', () => {
-            changeActiveTabs(index)
+            changeActiveTabs(index);
         })
     })
 
-    changeContent(0)
+    changeContent(0);
 }
 
-tabsFunc()
+tabsFunc();
